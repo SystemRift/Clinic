94e5675a0bfdd41829dbf78911fd50703e2d44a5
Müller Reinhard <73694790+SystemRift@users.noreply.github.com>
2021-04-30 06:28:29 +0300

    Add files via upload

---
+++ PC_App/Windows/Doctor_Desktop.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace PC_App.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для Doctor_Desktop.xaml
+    /// </summary>
+    public partial class Doctor_Desktop : Window
+    {
+        public Doctor_Desktop()
+        {
+            InitializeComponent();
+            startclock();
+        }
+
+        private void startclock()
+        {
+            //throw new NotImplementedException();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += tickevent;
+            timer.Start();
+        }
+
+        private void tickevent(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+            RlTime.Text = DateTime.Now.ToString(@"hh\:mm\:ss");
+        }
+
+        private void Btn_PList(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PC_App/Windows/Reception_Desktop.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace PC_App.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для Desktop.xaml
+    /// </summary>
+    public partial class Reception_Desktop : Window
+    {
+        public Reception_Desktop()
+        {
+            InitializeComponent();
+            startclock();
+        }
+
+        private void startclock()
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += tickevent;
+            timer.Start();
+        }
+
+        private void tickevent(object sender, EventArgs e)
+        {
+            RlTime.Text = DateTime.Now.ToString(@"hh\:mm\:ss");
+        }
+
+        private void Btn_TTable(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_PList(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Windows.Pages.PatientList();
+        }
+
+        private void Btn_SubP(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
